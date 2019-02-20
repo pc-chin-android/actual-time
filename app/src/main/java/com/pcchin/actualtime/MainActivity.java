6bcab8e593edd02d5d3b012b123d9d17ac902ef7
PC Chin <chin.pengcheng@gmail.com>
2019-02-20 19:35:41 +0800

    Final commit

--- app/src/main/java/com/pcchin/actualtime/MainActivity.java
+++ app/src/main/java/com/pcchin/actualtime/MainActivity.java
@@ -1,16 +1,34 @@
 package com.pcchin.actualtime;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
 import android.os.Handler;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnSuccessListener;
+
+import java.util.Locale;
+
 public class MainActivity extends AppCompatActivity {
+    private FusedLocationProviderClient fusedLoc;
+    private LocationRequest locationRequest;
     private boolean doubleBackToExitPressedOnce = false;
     private TimeThread timeThread;
-    private LocationThread locationThread;
+    private LocationCallback locationCallback;
     // CurrentLong must not be valid val (-180 < currentVal < 180)
     double currentLong = 1000;
 
@@ -18,46 +36,57 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        fusedLoc = LocationServices.getFusedLocationProviderClient(this);
+
+        locationRequest = LocationRequest.create();
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+        locationRequest.setInterval(5000);
+
+        locationCallback = new LocationCallback() {
+            @Override
+            public void onLocationResult(LocationResult locationResult) {
+                if (locationResult != null && locationResult.getLastLocation() != null) {
+                    currentLong = locationResult.getLastLocation().getLongitude();
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            TextView longVal = findViewById(R.id.long_int);
+                            longVal.setText(String.format(Locale.ENGLISH, "%s", currentLong));
+                        }
+                    });
+                }
+            }
+        };
     }
 
     @Override
     public void onPause() {
         super.onPause();
 
-        boolean retryTime = true;
-        while (retryTime) {
-            try {
-                this.timeThread.setRunning(false);
-                this.timeThread.join();
-                retryTime = false;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
-
-        boolean retryLoc = true;
-        while (retryLoc) {
-            try {
-                this.locationThread.setRunning(false);
-                this.locationThread.join();
-                retryLoc = false;
-            } catch (InterruptedException e) {
-                e.printStackTrace();
-            }
-        }
+        fusedLoc.removeLocationUpdates(locationCallback);
+        this.timeThread.setRunning(false);
     }
 
     @Override
     public void onResume() {
         super.onResume();
 
+        // Check permission
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED
+                && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION)
+                != PackageManager.PERMISSION_GRANTED) {
+            String[] permissionList = new String[]{Manifest.permission.ACCESS_FINE_LOCATION,
+                    Manifest.permission.ACCESS_COARSE_LOCATION};
+            ActivityCompat.requestPermissions(this, permissionList,100);
+        } else {
+            this.setLastLong();
+        }
+
         this.timeThread = new TimeThread(this);
         this.timeThread.setRunning(true);
         this.timeThread.start();
-
-        this.locationThread = new LocationThread(this);
-        this.locationThread.setRunning(true);
-        this.locationThread.start();
     }
 
     public void onAbtBtnSelected(View view) {
@@ -79,11 +108,53 @@ public class MainActivity extends AppCompatActivity {
 
                 @Override
                 public void run() {
-                    doubleBackToExitPressedOnce=false;
+                    doubleBackToExitPressedOnce = false;
                 }
             }, 1500);
 
         }
     }
 
+    @SuppressLint("MissingPermission")
+    private void setLastLong() {
+        // Permission checked in this.getLastLong
+        if (currentLong < -180 && currentLong > 180) {
+            fusedLoc.getLastLocation()
+                    .addOnSuccessListener(this, new OnSuccessListener<Location>() {
+                        @Override
+                        public void onSuccess(Location location) {
+                            // Got last known location. In some rare situations this can be null.
+                            if (location != null) {
+                                // Logic to handle location object
+                                currentLong = location.getLongitude();
+                                runOnUiThread(new Runnable() {
+                                    @Override
+                                    public void run() {
+                                        TextView longVal = findViewById(R.id.long_int);
+                                        longVal.setText(String.format(Locale.ENGLISH, "%s", currentLong));
+                                    }
+                                });
+                            }
+                        }
+                    });
+        }
+        fusedLoc.requestLocationUpdates(locationRequest, locationCallback, null);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions,
+                                           @NonNull int[] grantResults) {
+        switch(requestCode) {
+            case 100:
+                // getLastLong
+                if (grantResults.length > 0
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    this.setLastLong();
+                } else {
+                    Toast.makeText(this, "Location permission not granted. " +
+                            "Please enable the permission and try again.", Toast.LENGTH_SHORT).show();
+                }
+                break;
+        }
+    }
 }
--- app/src/main/java/com/pcchin/actualtime/TimeThread.java
+++ app/src/main/java/com/pcchin/actualtime/TimeThread.java
@@ -26,15 +26,34 @@ class TimeThread extends Thread {
                     sysTime.setText(String.format(Locale.ENGLISH, "%02d:%02d:%02d",
                             calendar.get(Calendar.HOUR_OF_DAY), calendar.get(Calendar.MINUTE), calendar.get(Calendar.SECOND)));
 
-                    if (mainActivity.currentLong >= -180 && mainActivity.currentLong < 180) {
-                        // TODO: Update coordinates
+                    if (mainActivity.currentLong >= -180 && mainActivity.currentLong <= 180) {
+                        // Calculate current time in secs based on current coordinates
+                        double currentSecs = (calendar.get(Calendar.HOUR_OF_DAY) * 60 * 60) +
+                                (calendar.get(Calendar.MINUTE) * 60) + calendar.get(Calendar.SECOND);
+                        double diffSecs = (mainActivity.currentLong / 180) * 60 * 60 * 12;
+                        currentSecs += diffSecs;
+                        if (currentSecs < 0) {
+                            currentSecs += 60 * 60 * 24;
+                        }
+
+                        // Convert them into hours, mins, secs
+                        int[] currentTimeArray = convertTime(currentSecs);
+                        int[] diffTimeArray = convertTime(diffSecs);
+
+                        // Set time
+                        TextView actTime = mainActivity.findViewById(R.id.act_time_int);
+                        actTime.setText(String.format(Locale.ENGLISH, "%02d:%02d:%02d",
+                                currentTimeArray[0], currentTimeArray[1], currentTimeArray[2]));
+                        TextView diffTime = mainActivity.findViewById(R.id.diff_time_int);
+                        diffTime.setText(String.format(Locale.ENGLISH, "%03d:%02d:%02d",
+                                diffTimeArray[0], diffTimeArray[1], diffTimeArray[2]));
                     }
                 }
             });
             try {
                 long sleepTime = 1000 + tempTime - (System.nanoTime() / 1000);
                 if (sleepTime > 0) {
-                    sleep(1000 - (System.nanoTime() / 1000) + tempTime);
+                    sleep(sleepTime);
                 }
             } catch (InterruptedException e) {
                 e.printStackTrace();
@@ -44,5 +63,16 @@ class TimeThread extends Thread {
 
     void setRunning(boolean running) {
         this.running = running;
+    }
+
+    private static int[] convertTime(double secs) {
+        int[] returnInt = new int[3];
+        returnInt[0] = (int) Math.floor(secs / 60 / 60);
+        returnInt[1] = (int) Math.floor((secs - returnInt[0] * 60 * 60) / 60);
+        returnInt[2] = (int) Math.floor(secs - (returnInt[0] * 60 * 60) - (returnInt[1] * 60));
+        if (returnInt[0] >= 24) {
+            returnInt[0] -= 24;
+        }
+        return returnInt;
     }
 }
